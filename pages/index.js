fdc058695446a52f1c4a76563e99aa68b31ca7a1
Master7130 <bxm7130@g.coppellisd.com>
2022-04-04 11:04:29 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import styles from './index.module.css';
 import Link from 'next/link';
 
@@ -11,6 +10,7 @@ export default function Home() {
         <meta name="description" content="Unleash your creativity" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      
       <div className={styles.banner}>
         <div className={styles.banner_overlay}></div>
         <div className={styles.banner_content}>
