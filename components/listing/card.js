704529eb0f707d9d992cd1a176ef9c7556d2c463
Mihir <mihirsuks@gmail.com>
2022-04-09 16:41:20 -0500

    make detailed listing a bit better

--- components/listing/card.js
+++ components/listing/card.js
@@ -3,16 +3,18 @@ import Link from 'next/link';
 
 export default function ListingCard(props) {
   return (
-    <Link href={props.url}>
-      <a>
-        <div className={styles.card}>
-          <div className={styles.card_img} style={{ backgroundImage: "url(" + props.img + ")" }}>
+    <div className={styles.card_container}>
+      <Link href={props.url}>
+        <a>
+          <div className={styles.card}>
+            <div className={styles.card_img} style={{ backgroundImage: "url(" + props.img + ")" }}>
+            </div>
+            <div className={styles.card_info}>
+              <p>{props.name}</p>
+            </div>
           </div>
-          <div className={styles.card_info}>
-            <p>{props.name}</p>
-          </div>
-        </div>
-      </a>
-    </Link>
+        </a>
+      </Link>
+    </div>
   );
 }
--- pages/listing/[listing].js
+++ pages/listing/[listing].js
@@ -2,12 +2,13 @@ import styles from './listing.module.css';
 import { MongoClient } from 'mongodb';
 import { ObjectID } from 'bson';
 import Meta from '../../components/meta';
+import filter_data from '../../data/filters.json';
 
 export async function getServerSideProps({ params }) {
     const client = await MongoClient.connect(process.env.MONGODB_URI);
     if (!client) {
         return {
-          props: {}  
+          props: {}
         };
     }
     const db = client.db('listings');
@@ -17,7 +18,7 @@ export async function getServerSideProps({ params }) {
     if (!data) {
         console.log('Listing not found');
         return {
-            props: {}  
+            props: {}
           };
     }
 
@@ -34,16 +35,34 @@ export async function getServerSideProps({ params }) {
 export default function Listing(props) {
     console.log(props);
 
+    const amenities_html = [];
+    props.listing.amenities.forEach((amenity) => {
+        let filter = filter_data.filters.find((f) => { return f.key === amenity; });
+        if (!filter) return;
+        amenities_html.push(<li key={amenity}>{filter.value}</li>);
+    });
+
     return (
         <div>
             <Meta title={props.listing.name} />
-            <h1>{props.listing.name}</h1>
+            <h1 className={styles.title}>{props.listing.name}</h1>
             <div className={styles.info_grid}>
                 <div className={styles.image} style={{backgroundImage: 'url(' + props.listing.image_url + ')'}}>
-
                 </div>
                 <div className={styles.description}>
                     <p>{props.listing.description}</p>
+                </div>
+                <div className={styles.amenities}>
+                    { props.listing.amenities.length > 0 ?
+                        <div>
+                            <h1>Amenities</h1>
+                            <ul>
+                                {amenities_html}
+                            </ul>
+                        </div>
+                    :
+                        <div></div>
+                    }
                 </div>
             </div>
         </div>
