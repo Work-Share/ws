3244ea05360c8c4ec80a76aa0e4be3cc7401d285
Bhavesh Mandalapu <mandalapubhavesh@gmail.com>
2022-04-25 20:19:31 -0500

    Add listing

--- components/dashboard/host.js
+++ components/dashboard/host.js
@@ -1,8 +1,124 @@
+import React, { useState } from 'react';
 import styles from './host.module.css';
-// import ListingCard from '../listing/card';
 import ListedPropertiesCard from './cards/dashboardCards';
+import { useSession } from 'next-auth/react';
+import Modal from 'react-modal';
+import { useRouter } from 'next/router';
+
+function AddListingModal(props, open, close) {
+    const { data: session, status } = useSession();
+    var email
+
+    if (session) email = session.user.email
+
+    const router = useRouter()
+
+    const [name, setName] = useState("");
+    const [closeDate, setCloseDate] = useState("");
+    const [openDate, setOpenDate] = useState("");
+    const [imageUrl, setImageUrl] = useState("");
+    const [address, setAddress] = useState("");
+    const [price, setPrice] = useState(0);
+    const [error, setError] = useState("");
+
+    const addListing = () => {
+        let search_params = new URLSearchParams();
+        search_params.append('email', email);
+        search_params.append('name', name);
+        search_params.append('open_date', openDate);
+        search_params.append('close_date', closeDate);
+        search_params.append('image_url', imageUrl);
+        search_params.append('address', address);
+        search_params.append('price', price);
+
+        const res = fetch('/api/dashboard/addListing?' + search_params.toString(), {
+            method: 'GET'
+        })
+        .then(() => router.reload())
+    }
+
+    return (
+        <Modal
+            isOpen={props.open}
+            onRequestClose={props.close}
+            contentLabel="Manage"
+            className={styles.modal_container}
+        >
+            <div className={styles.modal_content}>
+                <h1 className={styles.form_title}>Upload a listing</h1>
+                <form action="" method="get">
+                    <div className={styles.input_group}>
+                        <label htmlFor="name">Name</label>
+                        <input type="text" name="name" id="name" placeholder="Name of the workspace" onChange={e => {
+                            const newName = e.target.value;
+                            setName(newName);
+                        }} required />
+                    </div>
+
+                    <div className={styles.input_group}>
+                        <label htmlFor="open_date">Open Date</label>
+                        <input type="date" name="open_date" id="open_date" onChange={e => {
+                            const newOpenDate = e.target.value;
+                            if (e.target.value > closeDate) {
+                                setCloseDate(e.target.value);
+                            }
+                            setOpenDate(newOpenDate);
+                        }} required />
+                    </div>
+
+                    <div className={styles.input_group}>
+                        <label htmlFor="close_date">Close Date</label>
+                        <input type="date" name="close_date" id="close_date" onChange={e => {
+                            const newCloseDate = e.target.value;
+                            setCloseDate(newCloseDate);
+                        }} required />
+                    </div>
+
+                    <div className={styles.input_group}>
+                        <label htmlFor="image">Image url</label>
+                        <input type="url" name="for" id="for" placeholder="Image url (ends with .jpg or .png)" onChange={e => {
+                            const newImageUrl = e.target.value;
+                            setImageUrl(newImageUrl);
+                        }} required />
+                    </div>
+
+                    <div className={styles.input_group}>
+                        <label htmlFor="address">Address</label>
+                        <input type="text" name="address" id="address" placeholder="Street address" onChange={e => {
+                            const newAddress = e.target.value;
+                            setAddress(newAddress);
+                        }} required />
+                    </div>
+
+                    <div className={styles.input_group}>
+                        <label htmlFor="price">Price</label>
+                        <input type="number" name="price" id="price" min="0" onChange={e => {
+                            const newPrice = e.target.value;
+                            setPrice(newPrice);
+                        }} required />
+                    </div>
+                </form>
+                <div>
+                    <input type="button" value="Upload" onClick={addListing} className={styles.submit_button} />
+                </div>
+                {/* <p className={styles.error}>{error}</p> */}
+            </div>
+        </Modal>
+    )
+}
 
 export default function Host(props) {
+    // Modal
+    const [modalIsOpen, setIsOpen] = useState(false)
+
+    function openModal() {
+        setIsOpen(true);
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+    }
+
     return (
         <div>
             <div className={styles.section}>
@@ -19,6 +135,9 @@ export default function Host(props) {
                         :
                         <p><i>You have not posted any properties</i></p>
                 }
+
+                <button onClick={() => setIsOpen(true)}>Add Listing</button>
+                <AddListingModal open={modalIsOpen} close={closeModal} />
             </div>
         </div>
     );
---
+++ pages/api/dashboard/addListing.js
@@ -0,0 +1,36 @@
+import { MongoClient } from 'mongodb';
+
+export default async function handler(req, res) {
+    const client = await MongoClient.connect(process.env.MONGODB_URI);
+    const db = client.db("listings");
+    const query = req.query
+
+    let open_date = new Date(query.open_date);
+    let close_date = new Date(query.close_date);
+
+    await db.collection("listings").insert({
+        name: query.name,
+        open_date: open_date,
+        close_date: close_date,
+        image_url: query.image_url,
+        address: query.address,
+        rating: 0,
+        price: query.price,
+        amenities: []
+        // amenities: req.query.amenities
+    })
+
+    const db2 = client.db("listings");
+    await db2.collection("listings").createIndex({ "$**": "text" }, { name: "TextIndex" })
+
+    const listing = await db2.collection("listings").find({ name: { $eq: query.name } }).toArray();
+    const listingData = JSON.parse(JSON.stringify(listing));
+
+    const db3 = client.db("Users");
+    const data = await db3.collection("UserData").updateOne(
+        { email: query.email },
+        { $push: { listedProperties: listingData[0]._id } }
+    );
+
+    res.status(200).json({ x: listingData[0]._id })
+}
