516c296b9352b4cbc916c52960525f79b571c6ba
msc2913 <msc2913@g.coppellisd.com>
2022-04-28 11:55:58 -0500

    forgot about these

--- pages/about.js
+++ pages/about.js
@@ -3,10 +3,10 @@ export default function About() {
         <div>
             <div style={{ 'padding': '32px' }}>
                 <h1>About us</h1>
-                <p>
+                <p style={{ 'padding': '32px' }}>
                 At WorkShare, we aim to help you achieve your creative goals by helping you find the space you need, for cheap.
                 </p>
-                <p>
+                <p style={{ 'padding': '32px' }}>
                 We at the WorkShare team are committed to providing our partners, hosts, and renters with an ideal environment to make sure they have the best experience possible. Property owners who wish to enable others to lease their workspace can use our platform to reach thousands of potential customers. Conversely, creatives can use our platform to find the ideal workspace for them to get started with their creative endeavors. But what is a workspace? A workspace is anywhere our customers can work on their projects and these workspaces are provided by our partners who we call hosts.
                 </p>
             </div>
