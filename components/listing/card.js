b38b71748d8a9a936778098d71642472bbfb6eb6
msc2913 <msc2913@g.coppellisd.com>
2022-05-03 11:16:28 -0500

    finish checkout

--- components/listing/card.js
+++ components/listing/card.js
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export default function ListingCard(props) {
   return (
     <div className={styles.card_container}>
-      <Link href={props.url}>
+      <Link href={{ pathname: props.url, query: { 'checkin':  props.check_in, 'checkout': props.check_out } }}>
         <a>
           <div className={styles.card}>
             <div className={styles.card_img} style={{ backgroundImage: "url(" + props.img + ")" }}>
